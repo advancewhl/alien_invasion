ddf10fbd3c3302c722ce48635e0a52d92f9e5fe8
whloong <2456412172@qq.com>
2025-11-07 21:09:33 +0800

    修复飞船图片加载、缩放，并新增缓存忽略与 800x600 设置

--- ship.py
+++ ship.py
@@ -5,10 +5,13 @@ class Ship(object):
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取取外接矩形
-        self.image = pygame.image.load("image/ship,,bmp")
+        self.image = pygame.image.load("images/ship.bmp").convert_alpha()
+        self.image = pygame.transform.smoothscale(
+            self.image, (80, 80)
+        )  # 宽 80、高 120，可按需求修改
         self.rect = self.image.get_rect()
 
         # 每个新飞船都在底部中央
@@ -16,4 +19,4 @@ class Ship(object):
 
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.iamge, self.rect)
+        self.screen.blit(self.image, self.rect)
